a01c384661b74e34f00904d075f9843b7e00e409
Mikaela Caron <mikaelacaron@gmail.com>
2023-07-28 01:08:07 -0400

    Tutorial steps

--- SupaFeatures/ViewModel.swift
+++ SupaFeatures/ViewModel.swift
@@ -17,6 +17,7 @@ enum AuthAction: String, CaseIterable {
     case signIn = "Sign In"
 }
 
+@MainActor
 final class ViewModel: ObservableObject {
     
     @Published var isAuthenticated = false
@@ -33,31 +34,87 @@ final class ViewModel: ObservableObject {
     // MARK: - Database
     
     func createFeatureRequest(text: String) async throws {
+        let user = try await supabase.auth.session.user
+        
+        let feature = Feature(createdAt: Date(), text: text, isComplete: false, userID: user.id)
+        
+        try await supabase.database
+            .from(Table.features)
+            .insert(values: feature)
+            .execute()
     }
     
     func fetchFeatureRequests() async throws {
+        let features: [Feature] = try await supabase.database
+            .from(Table.features)
+            .select()
+            .order(column: "created_at", ascending: false)
+            .execute()
+            .value
+        
+        DispatchQueue.main.async {
+            self.features = features
+        }
     }
     
     func update(_ feature: Feature, with text: String) async {
+        guard let id = feature.id else {
+            print("❌ Can't update feature \(feature.id)")
+            return
+        }
+        
+        var toUpdate = feature
+        toUpdate.text = text
+        
+        do {
+            try await supabase.database
+                .from(Table.features)
+                .update(values: toUpdate)
+                .eq(column: "id", value: id)
+                .execute()
+        } catch {
+            print("❌ Error: \(error)")
+        }
     }
     
     func deleteFeature(at id: Int) async throws {
+        try await supabase.database
+            .from(Table.features)
+            .delete()
+            .eq(column: "id", value: id)
+            .execute()
     }
     
     // MARK: - Authentication
     
     func signUp() async throws {
+        let response = try await supabase.auth.signUp(email: email, password: password)
     }
     
     func signIn() async throws {
+        let session = try await supabase.auth.signIn(email: email, password: password)
     }
     
     func isUserAuthenticated() async {
+        do {
+            _ = try await supabase.auth.session.user
+            isAuthenticated = true
+        } catch {
+            isAuthenticated = false
+        }
     }
     
     func signOut() async throws {
+        try await supabase.auth.signOut()
+        isAuthenticated = false
     }
     
     func authorize() async throws {
+        switch authAction {
+        case .signUp:
+            try await signUp()
+        case .signIn:
+            try await signIn()
+        }
     }
 }
